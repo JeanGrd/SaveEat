d5d226435580c0065b3a29c190f6fa79b53391b8
jean <jean.gd@outlook.fr>
2023-05-06 21:03:10 +0200

    new version

--- src/app/notre-mission/notre-mission.component.ts
+++ src/app/notre-mission/notre-mission.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notre-mission',
   templateUrl: './notre-mission.component.html',
   styleUrls: ['./notre-mission.component.css']
 })
-export class NotreMissionComponent {
+export class NotreMissionComponent implements OnInit {
+  carouselItems: HTMLElement[];
+  currentItemIndex: number;
 
+  constructor() {
+    this.currentItemIndex = 0;
+    this.carouselItems = [];
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.carouselItems = Array.from(document.querySelectorAll('.carousel-item'));
+      this.startCarousel();
+    }, 0);
+  }
+
+
+  startCarousel() {
+    setInterval(() => {
+      this.carouselItems[this.currentItemIndex].classList.remove('active');
+      this.currentItemIndex = (this.currentItemIndex + 1) % this.carouselItems.length;
+      this.carouselItems[this.currentItemIndex].classList.add('active');
+    }, 3000); // Durata în milisecunde între schimbarea imaginilor
+  }
 }
